f25365d2fa8f8fc77e784b3e5bad2e4046a3de8c
samuelfer <samuelfesant@gmail.com>
2022-06-04 15:37:53 -0300

    Add conexão com o banco de dados

---
+++ src/shared/typeorm/index.ts
@@ -0,0 +1,18 @@
+import { DataSource } from 'typeorm';
+
+const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'postgres',
+  database: 'control_stock',
+});
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log('Data Source has been initialized!');
+  })
+  .catch(err => {
+    console.error('Error during Data Source initialization', err);
+  });
