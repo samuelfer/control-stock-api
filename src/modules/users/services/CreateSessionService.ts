a9164c377b52278dcff3179c663ee57984517a69
samuelfer <samuelfesant@gmail.com>
2022-06-05 17:59:40 -0300

    Configure response de login com token jwt

--- src/modules/users/services/CreateSessionService.ts
+++ src/modules/users/services/CreateSessionService.ts
@@ -1,12 +1,17 @@
 import AppError from '@shared/errors/AppError';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 import { getCustomRepository } from 'typeorm';
 import User from '../typeorm/entities/User';
 import IRequest from '../typeorm/interface/IRequestSession';
+import IResponseSession from '../typeorm/interface/IResponseSession';
 import UserRepository from '../typeorm/repositories/UserRepository';
 
 class CreateSessionService {
-  public async execute({ email, password }: IRequest): Promise<User> {
+  public async execute({
+    email,
+    password,
+  }: IRequest): Promise<IResponseSession> {
     const userRepository = getCustomRepository(UserRepository);
     const user = await userRepository.findByEmail(email);
 
@@ -20,7 +25,15 @@ class CreateSessionService {
       throw new AppError('Incorrect email/password combination.', 401);
     }
 
-    return user;
+    const token = sign({}, '79fea6ea9ca60af673521686d1e4d2e4', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return {
+      user,
+      token,
+    };
   }
 }
 
--- src/modules/users/typeorm/interface/IResponseSession.ts
+++ src/modules/users/typeorm/interface/IResponseSession.ts
@@ -2,6 +2,7 @@ import User from '../entities/User';
 
 interface IResponse {
   user: User;
+  token: string;
 }
 
 export default IResponse;
