8041a6669f2687dfb59a6f84282a7f5b5cfaa53d
samuelfer <samuelfesant@gmail.com>
2022-06-05 17:41:34 -0300

    Add routes para autenticação de usuários

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,4 +1,5 @@
 import productRouter from '@modules/products/routes/product.routes';
+import sessionRouter from '@modules/users/routes/session.routes';
 import userRouter from '@modules/users/routes/user.routes';
 import { Router } from 'express';
 
@@ -6,5 +7,6 @@ const routes = Router();
 
 routes.use('/products', productRouter);
 routes.use('/users', userRouter);
+routes.use('/sessions', sessionRouter);
 
 export default routes;
