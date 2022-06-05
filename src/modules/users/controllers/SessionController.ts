a8115656c6bfee9ec70607e395cfe920b3d81ce6
samuelfer <samuelfesant@gmail.com>
2022-06-05 17:37:21 -0300

    Add método create session no controller

---
+++ src/modules/users/controllers/SessionController.ts
@@ -0,0 +1,19 @@
+import { Request, Response, response } from 'express';
+import CreateSessionService from '../services/CreateSessionService';
+
+class SessionController {
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { email, password } = request.body;
+
+    const createSession = new CreateSessionService();
+
+    const user = await createSession.execute({
+      email,
+      password,
+    });
+
+    return response.json(user);
+  }
+}
+
+export default SessionController;
