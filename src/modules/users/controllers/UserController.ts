6bb9ccc41dc2513fd640b0255dd607b0e668fcbe
samuelfer <samuelfesant@gmail.com>
2022-06-05 14:44:26 -0300

    Add método para cadastrar usuário no controller

--- src/modules/users/controllers/UserController.ts
+++ src/modules/users/controllers/UserController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import CreateUserService from '../services/CreateUserService';
 import ListUserService from '../services/ListUserService';
 
 class UserController {
@@ -8,6 +9,20 @@ class UserController {
     const users = await listUsers.execute();
 
     return response.json(users);
+  }
+
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { name, email, password } = request.body;
+
+    const createUser = new CreateUserService();
+
+    const user = await createUser.execute({
+      name,
+      email,
+      password,
+    });
+
+    return response.json(user);
   }
 }
 
