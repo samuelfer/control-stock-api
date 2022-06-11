7af4a555b03b274492360d57bc12f695c496c382
samuelfer <samuelfesant@gmail.com>
2022-06-11 16:41:56 -0300

    Add controller de pedido

---
+++ src/modules/orders/controllers/OrderController.ts
@@ -0,0 +1,56 @@
+import { Request, Response } from 'express';
+import CreateOrderService from '../services/CreateOrderService';
+import ShowOrderService from '../services/ShowOrderService';
+
+class OrderController {
+  public async show(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+
+    const showOrder = new ShowOrderService();
+
+    const order = await showOrder.execute({ id });
+
+    return response.json(order);
+  }
+
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { customer_id, products } = request.body;
+
+    const createOrder = new CreateOrderService();
+
+    const order = await createOrder.execute({
+      customer_id,
+      products,
+    });
+
+    return response.json(order);
+  }
+
+  public async update(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+    const { name, price, quantity } = request.body;
+
+    const updateProduct = new UpdateProductService();
+
+    const product = await updateProduct.execute({
+      id,
+      name,
+      price,
+      quantity,
+    });
+
+    return response.json(product);
+  }
+
+  public async delete(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+
+    const deleteProduct = new DeleteProductService();
+
+    await deleteProduct.execute({ id });
+
+    return response.json([]);
+  }
+}
+
+export default OrderController;
